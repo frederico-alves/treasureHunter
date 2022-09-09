29dd62c72643ef7f877158e65dc27aabc02753d8
frederico-alves <alves.pt.dk@gmail.com>
2022-09-09 11:41:34 +0200

    solved

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # treasureHunter
+
+TreasureHunter.zip is a partially solved Treasure Hunter solution. It shows a map and shows a coin at random position on the map.
+
+Your task is to:
+✅• Download the solution
+✅• Discuss and try to understand the solution with another student
+✅• Show from 1 to 10 coins randomly on the map i.e. there should be shown
+1/2/3 or ...10 coin/coins each time the browser is refreshed.
+✅• Create a function that returns a random number between min and max ✅• You may refactor the solution and make the code better.
--- app.js
+++ app.js
@@ -3,6 +3,17 @@ $ =(queryString) => document.querySelector(queryString);
 $('body').style.backgroundImage = 'url(map.jpg)';
 $('body').style.backgroundRepeat = 'no-repeat';
 
+
+// Returns a random integer from 0 to 10:
+// let randomNumber = Math.floor(Math.random() * 11);
+
+// A function that returns a random number between min and max 
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Add coins randomly in the map
+for (let i = 0; i < getRandomNumber(2, 30); i++) {
     let leftMargin = Math.floor(Math.random() * (Math.floor(650) - Math.ceil(10) + 1)) + Math.ceil(10);
     let topMargin = Math.floor(Math.random() * (Math.floor(400) - Math.ceil(10) + 1)) + Math.ceil(10);
     let div = document.createElement('div');
@@ -14,9 +25,7 @@ $('body').style.backgroundRepeat = 'no-repeat';
     div.style.left = leftMargin;
     div.style.top = topMargin;
     $('body').appendChild(div);
-
-
-
+}
 
 
 
